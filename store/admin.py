8a18ba99d6f327a795404b190ca0cda4adbe12d9
Phever <3782319+phever@users.noreply.github.com>
2025-06-19 03:01:29 -0600

    add gitignore and default order

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from store.models import StoreItem
 
-# Register your models here.
-admin.site.register(StoreItem)
+
+@admin.register(StoreItem)
+class StoreItemAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "default_order"]
