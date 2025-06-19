b31ff350d71b232a0f77d70f2eb25b3750a8cf0e
Phever <3782319+phever@users.noreply.github.com>
2025-06-19 04:11:24 -0600

    center items and text

--- static/preload.js
+++ static/preload.js
@@ -1,25 +1,24 @@
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-    localStorage.theme = 'dark';
-} else {
-    document.documentElement.classList.remove('dark');
-    localStorage.theme = 'light';
-}
+// On page load or when changing themes, best to add inline in `head` to avoid
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
 
 // load functions
 function toggleDarkMode() {
-    if (localStorage.theme === 'dark') {
-        document.documentElement.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
-    else if (localStorage.theme === 'light') {
-        document.documentElement.classList.add('dark');
-        localStorage.theme = 'dark';
-    }
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  } else if (localStorage.theme === "light") {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  }
 }
 
 function toggleModal() {
-    document.getElementById('content-block').classList.toggle('blur-sm');
-    document.getElementById('modal').classList.toggle('hidden');
-    //document.getElementById('popup').classList.toggle('hidden');
+  document.getElementById("content-block").classList.toggle("blur-sm");
+  document.getElementById("modal").classList.toggle("hidden");
+  //document.getElementById('popup').classList.toggle('hidden');
 }
