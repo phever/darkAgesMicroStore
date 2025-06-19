5ca5206dc570f3e5a86f0634207e2fa460a3f382
Phever <3782319+phever@users.noreply.github.com>
2025-06-19 00:34:09 -0600

    update to add usd

--- static/index.js
+++ static/index.js
@@ -5,7 +5,7 @@ const USD_STRING = " USD";
 DataTable.type("da-price", {
   detect: function (data) {
     return typeof data === "string" &&
-      data.match(/(\d+)( Million Gold| Code| USD\(s\))/)
+      data.match(/(\d+)( Million Gold| Code\(s\)| USD)/)
       ? "da-price"
       : null;
   },
@@ -15,7 +15,7 @@ DataTable.type("da-price", {
         return Number(data.replace(CODE_STRING, "")) * 1000;
       }
       if (data.includes(USD_STRING)) {
-        return Number(data.replace(USD_STRING, "") * 1000);
+        return Number(data.replace(USD_STRING, "") * 100);
       }
       return Number(data.replace(GOLD_STRING, ""));
     },
