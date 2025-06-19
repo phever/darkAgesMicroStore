635ebec6dfcc657f59861b6b60a70331f1082852
phever <3782319+phever@users.noreply.github.com>
2025-06-18 21:09:49 -0600

    update deps and add usd

--- static/index.js
+++ static/index.js
@@ -1,29 +1,29 @@
-const CODE_STRING = ' Code(s)'
-const GOLD_STRING = ' Million Gold'
+const CODE_STRING = " Code(s)";
+const GOLD_STRING = " Million Gold";
+const USD_STRING = " USD";
 
-DataTable.type('da-price', {
-    detect: function (data) {
-        return typeof data === 'string' &&
-        data.match(/(\d+)( Million Gold| Code\(s\))/)
-            ? 'da-price'
-            : null;
+DataTable.type("da-price", {
+  detect: function (data) {
+    return typeof data === "string" &&
+      data.match(/(\d+)( Million Gold| Code| USD\(s\))/)
+      ? "da-price"
+      : null;
+  },
+  order: {
+    pre: function (data) {
+      if (data.includes(CODE_STRING)) {
+        return Number(data.replace(CODE_STRING, "")) * 1000;
+      }
+      if (data.includes(USD_STRING)) {
+        return Number(data.replace(USD_STRING, "") * 1000);
+      }
+      return Number(data.replace(GOLD_STRING, ""));
     },
-    order: {
-        pre: function (data) {
-            if (data.includes(CODE_STRING)) {
-                return Number(
-                    data.replace(CODE_STRING, '')
-                ) * 1000
-            }
-            return Number(
-                data.replace(GOLD_STRING, '')
-            );
-        }
-    },
-    className: 'dt-da-price'
+  },
+  className: "dt-da-price",
 });
 
-$('#shop-table').DataTable({
-    responsive: true,
-    order: [],
+$("#shop-table").DataTable({
+  responsive: true,
+  order: [],
 });
--- store/models.py
+++ store/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 
-UNITS_CHOICES = (('G', 'Million Gold'),
-                 ('C', 'Code(s)'))
+UNITS_CHOICES = (("G", "Million Gold"), ("C", "Code(s)"), ("D", "USD"))
 
 
 class StoreItem(models.Model):
@@ -12,24 +11,26 @@ class StoreItem(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     units = models.CharField(max_length=2, choices=UNITS_CHOICES)
     description = models.TextField()
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to="static/images/")
     visible = models.BooleanField(default=True)
     default_order = models.IntegerField(default=0)
 
     def get_units(self):
         if self.price == 0:
-            return ''
-        elif self.units == 'G':
-            return 'Million Gold'
-        elif self.units == 'C':
-            return 'Code(s)'
-        return ''
+            return ""
+        elif self.units == "G":
+            return "Million Gold"
+        elif self.units == "C":
+            return "Code(s)"
+        elif self.units == "D":
+            return "USD"
+        return ""
 
     def __str__(self):
         qty = self.qty
         if self.to_order:
-            qty = '∞'
-        visible = ''
+            qty = "∞"
+        visible = ""
         if not self.visible:
-            visible = 'HIDDEN - '
-        return f'{visible}{self.name} [{qty}] @ {self.price} {self.get_units()}'
+            visible = "HIDDEN - "
+        return f"{visible}{self.name} [{qty}] @ {self.price} {self.get_units()}"
